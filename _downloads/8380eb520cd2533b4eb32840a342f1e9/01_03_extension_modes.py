f08276a071b7ebae74c24908dca64552a2fd005c
pagarcia <pagarcia@users.noreply.github.com>
2025-12-09 11:12:06 +0000

    deploy: splineops/splineops@b69f483cac2d720bbc51fa311625809df2664e17

--- _downloads/8380eb520cd2533b4eb32840a342f1e9/01_03_extension_modes.py
+++ _downloads/8380eb520cd2533b4eb32840a342f1e9/01_03_extension_modes.py
@@ -3,7 +3,7 @@
 # sphinx_gallery_end_ignore
 
 """
-Extension modes
+Extension Modes
 ===============
 
 Plotting different extension modes of signals.
@@ -18,7 +18,7 @@ Plotting different extension modes of signals.
 
 import numpy as np
 import matplotlib.pyplot as plt
-from splineops.spline_interpolation.tensorspline import TensorSpline
+from splineops.spline_interpolation.tensor_spline import TensorSpline
 
 x_values = np.linspace(0, 6, 101)
 
